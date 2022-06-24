796770de00600fab746511f76ce8f873fa03d40b
RaenonX <raenonx0710@gmail.com>
2022-06-24 08:13:43 -0500

    Added Jun. 24 data
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- plot_from_data_diff.py
+++ plot_from_data_diff.py
@@ -21,8 +21,8 @@ from fop_analyze.utils import get_config, get_file_props
 # ESU22 American
 # > https://www.barchart.com/futures/quotes/ESU22/options?futuresOptionsView=split&moneyness=allRows
 
-MAIN_DATE: str = "06-23-2022"
-SUB_DATE: str = "06-22-2022"
+MAIN_DATE: str = "06-24-2022"
+SUB_DATE: str = "06-23-2022"
 
 DATA_PATHS: list[(str, str)] = [
     (
